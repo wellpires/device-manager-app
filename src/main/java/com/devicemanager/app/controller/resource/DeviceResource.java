cb1fffd6ffe12c1cab8575f39c7c6c8509df5e5a
Wellington Gonçalves Pires <wellington.goncalves@live.com>
2025-01-17 22:40:24 -0300

    Added fetch all devices API documentation

--- src/main/java/com/devicemanager/app/controller/resource/DeviceResource.java
+++ src/main/java/com/devicemanager/app/controller/resource/DeviceResource.java
@@ -1,9 +1,12 @@
 package com.devicemanager.app.controller.resource;
 
+import com.devicemanager.app.dto.DeviceDTO;
 import com.devicemanager.app.dto.request.DeviceRequest;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 
 public interface DeviceResource {
@@ -13,4 +16,11 @@ public interface DeviceResource {
             @ApiResponse(responseCode = "204", description = "Device created!"),
             @ApiResponse(responseCode = "500", description = "Internal server error!")})
     ResponseEntity<Void> create(DeviceRequest deviceRequest);
+
+    @Operation(summary = "Fetch all Devices")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Fetch all devices!"),
+            @ApiResponse(responseCode = "500", description = "Internal server error!")})
+    Page<DeviceDTO> list(Pageable pageable);
+
 }
