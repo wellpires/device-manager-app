fc053eb383d8b8b1e35dd7e05ca7b6cdb24b1618
Wellington Gonçalves Pires <wellington.goncalves@live.com>
2025-01-18 11:40:36 -0300

    Added DeviceNotFoundException handler into Controller advice to handle device not found exception

--- src/main/java/com/devicemanager/app/controller/DeviceControllerAdvice.java
+++ src/main/java/com/devicemanager/app/controller/DeviceControllerAdvice.java
@@ -1,8 +1,11 @@
 package com.devicemanager.app.controller;
 
 import com.devicemanager.app.dto.response.ErrorResponse;
+import com.devicemanager.app.exception.DeviceNotFoundException;
 import io.swagger.v3.oas.annotations.Hidden;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -23,6 +26,19 @@ public class DeviceControllerAdvice {
 
         return ResponseEntity.internalServerError()
                 .body(errorResponse);
+
+    }
+
+    @ExceptionHandler(DeviceNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleDeviceNotFoundException(DeviceNotFoundException dnfEx) {
+
+        log.error(dnfEx.getMessage(), dnfEx);
+
+        ErrorResponse errorResponse = ErrorResponse.builder()
+                .message(dnfEx.getMessage())
+                .build();
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
 
     }
 
