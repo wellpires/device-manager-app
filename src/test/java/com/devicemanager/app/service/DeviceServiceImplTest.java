af005067a18545fa9d7d83a2dbb1ed1f81cbe2e4
Wellington Gonçalves Pires <wellington.goncalves@live.com>
2025-01-18 12:34:58 -0300

    Written unit tests for delete devices methods

--- src/test/java/com/devicemanager/app/service/DeviceServiceImplTest.java
+++ src/test/java/com/devicemanager/app/service/DeviceServiceImplTest.java
@@ -2,6 +2,8 @@ package com.devicemanager.app.service;
 
 import com.devicemanager.app.dto.DeviceDTO;
 import com.devicemanager.app.entity.DeviceEntity;
+import com.devicemanager.app.enums.StateEnum;
+import com.devicemanager.app.exception.DeviceInUseException;
 import com.devicemanager.app.exception.DeviceNotFoundException;
 import com.devicemanager.app.repository.DeviceRepository;
 import org.apache.commons.lang3.RandomStringUtils;
@@ -28,7 +30,7 @@ import static org.hamcrest.Matchers.notNullValue;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.isNotNull;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class DeviceServiceImplTest {
@@ -102,6 +104,54 @@ class DeviceServiceImplTest {
         assertThrows(DeviceNotFoundException.class, () -> {
             deviceService.find(UUID.randomUUID());
         });
+    }
+
+    @Test
+    void shouldDeleteDevice() {
+
+        DeviceEntity deviceEntity = DeviceEntity.builder()
+                .id(UUID.randomUUID())
+                .name(RandomStringUtils.secure().nextAlphanumeric(10))
+                .brand(RandomStringUtils.secure().nextAlphanumeric(10))
+                .build();
+        when(deviceRepository.findById(any(UUID.class))).thenReturn(Optional.ofNullable(deviceEntity));
+
+        deviceService.delete(UUID.randomUUID());
+
+        verify(deviceRepository, times(1)).delete(any(DeviceEntity.class));
+
+    }
+
+    @Test
+    void shouldNotDeleteDeviceBecauseDeviceNotFound() {
+
+        when(deviceRepository.findById(any(UUID.class))).thenReturn(Optional.empty());
+
+        assertThrows(DeviceNotFoundException.class, () -> {
+            deviceService.delete(UUID.randomUUID());
+        });
+
+        verify(deviceRepository, never()).delete(any(DeviceEntity.class));
+
+    }
+
+    @Test
+    void shouldNotDeleteDeviceBecauseDeviceIsInUse() {
+
+        DeviceEntity deviceEntity = DeviceEntity.builder()
+                .id(UUID.randomUUID())
+                .name(RandomStringUtils.secure().nextAlphanumeric(10))
+                .brand(RandomStringUtils.secure().nextAlphanumeric(10))
+                .state(StateEnum.IN_USE)
+                .build();
+        when(deviceRepository.findById(any(UUID.class))).thenReturn(Optional.ofNullable(deviceEntity));
+
+        assertThrows(DeviceInUseException.class, () -> {
+            deviceService.delete(UUID.randomUUID());
+        });
+
+        verify(deviceRepository, never()).delete(any(DeviceEntity.class));
+
     }
 
 }
