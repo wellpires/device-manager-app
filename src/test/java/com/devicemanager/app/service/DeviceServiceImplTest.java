5a18730020b15089fc32d42bd137af6440670034
Wellington Gonçalves Pires <wellington.goncalves@live.com>
2025-01-17 22:52:04 -0300

    Written unit tests to fetch all devices method

--- src/test/java/com/devicemanager/app/service/DeviceServiceImplTest.java
+++ src/test/java/com/devicemanager/app/service/DeviceServiceImplTest.java
@@ -1,14 +1,29 @@
 package com.devicemanager.app.service;
 
 import com.devicemanager.app.dto.DeviceDTO;
+import com.devicemanager.app.entity.DeviceEntity;
 import com.devicemanager.app.repository.DeviceRepository;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class DeviceServiceImplTest {
@@ -28,6 +43,30 @@ class DeviceServiceImplTest {
                 .build();
 
         deviceService.create(deviceDTO);
+
+    }
+
+    @Test
+    void shouldList() {
+
+        List<DeviceEntity> deviceEntities = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            DeviceEntity deviceEntity = DeviceEntity.builder()
+                    .id(UUID.randomUUID())
+                    .name(RandomStringUtils.secure().nextAlphanumeric(10))
+                    .brand(RandomStringUtils.secure().nextAlphanumeric(10))
+                    .build();
+            deviceEntities.add(deviceEntity);
+
+        }
+
+        PageImpl<DeviceEntity> deviceDTOPage = new PageImpl<>(deviceEntities, Pageable.ofSize(10), 10);
+
+        when(deviceRepository.findAll(any(Pageable.class))).thenReturn(deviceDTOPage);
+
+        Page<DeviceDTO> pageDeviceDTOs = deviceService.list(Pageable.ofSize(10));
+
+        assertThat(pageDeviceDTOs.getTotalElements(), equalTo(10L));
 
     }
 }
