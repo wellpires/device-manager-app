2a326240f4783d51970678a457da887fe13083c7
Wellington Gonçalves Pires <wellington.goncalves@live.com>
2025-01-18 11:45:10 -0300

    Added find one device API endpoint

--- src/main/java/com/devicemanager/app/controller/DeviceController.java
+++ src/main/java/com/devicemanager/app/controller/DeviceController.java
@@ -3,6 +3,7 @@ package com.devicemanager.app.controller;
 import com.devicemanager.app.controller.resource.DeviceResource;
 import com.devicemanager.app.dto.DeviceDTO;
 import com.devicemanager.app.dto.request.DeviceRequest;
+import com.devicemanager.app.dto.response.DeviceResponse;
 import com.devicemanager.app.service.DeviceService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -46,4 +47,18 @@ public class DeviceController implements DeviceResource {
     public Page<DeviceDTO> list(Pageable pageable) {
         return deviceService.list(pageable);
     }
+
+    @Override
+    @GetMapping(path = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<DeviceResponse> find(@PathVariable("id") UUID id) {
+
+        DeviceDTO deviceDTO = deviceService.find(id);
+
+        DeviceResponse deviceResponse = DeviceResponse.builder()
+                .deviceDTO(deviceDTO)
+                .build();
+        return ResponseEntity.ok(deviceResponse);
+    }
+
+
 }
