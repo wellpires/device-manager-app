343b887d301179dffe3c28fa9dd6cccef76d13f9
Wellington Gonçalves Pires <wellington.goncalves@live.com>
2025-01-18 11:56:41 -0300

    Written unit tests to find one device endpoint

--- src/test/java/com/devicemanager/app/controller/DeviceControllerTest.java
+++ src/test/java/com/devicemanager/app/controller/DeviceControllerTest.java
@@ -2,6 +2,7 @@ package com.devicemanager.app.controller;
 
 import com.devicemanager.app.dto.DeviceDTO;
 import com.devicemanager.app.dto.request.DeviceRequest;
+import com.devicemanager.app.exception.DeviceNotFoundException;
 import com.devicemanager.app.service.DeviceService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.commons.lang3.RandomStringUtils;
@@ -16,11 +17,10 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.util.UriComponentsBuilder;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.UUID;
+import java.net.URI;
+import java.util.*;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
@@ -36,6 +36,7 @@ class DeviceControllerTest {
     private static final String BASE_URL = "/devices";
     private static final String POST_CREATE_DEVICES = BASE_URL.concat("");
     private static final String GET_DEVICES = BASE_URL.concat("");
+    private static final String GET_DEVICE = BASE_URL.concat("/{id}");
 
     @Autowired
     private ObjectMapper mapper;
@@ -93,5 +94,52 @@ class DeviceControllerTest {
                 .andExpect(jsonPath("$.content.length()").value(10))
                 .andDo(print())
                 .andReturn();
+    }
+
+    @Test
+    void shouldFindOneDevice() throws Exception {
+
+        DeviceDTO deviceDTO = DeviceDTO.builder()
+                .id(UUID.randomUUID())
+                .name(RandomStringUtils.secure().nextAlphanumeric(10))
+                .brand(RandomStringUtils.secure().nextAlphanumeric(10))
+                .build();
+
+        when(deviceService.find(any(UUID.class))).thenReturn(deviceDTO);
+
+        Map<String, UUID> param = new HashMap<>();
+        param.put("id", UUID.randomUUID());
+
+        URI getDeviceURI = UriComponentsBuilder.fromPath(GET_DEVICE)
+                .buildAndExpand(param)
+                .toUri();
+
+        mockMvc.perform(get(getDeviceURI).contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.device").isNotEmpty())
+                .andExpect(jsonPath("$.device.id").value(deviceDTO.id().toString()))
+                .andExpect(jsonPath("$.device.name").value(deviceDTO.name()))
+                .andExpect(jsonPath("$.device.brand").value(deviceDTO.brand()))
+                .andDo(print());
+
+    }
+
+    @Test
+    void shouldNotFindOneDevice() throws Exception {
+
+        when(deviceService.find(any(UUID.class))).thenThrow(new DeviceNotFoundException(UUID.randomUUID()));
+
+        Map<String, UUID> param = new HashMap<>();
+        param.put("id", UUID.randomUUID());
+
+        URI getDeviceURI = UriComponentsBuilder.fromPath(GET_DEVICE)
+                .buildAndExpand(param)
+                .toUri();
+
+        mockMvc.perform(get(getDeviceURI).contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.message").isNotEmpty())
+                .andDo(print());
+
     }
 }
