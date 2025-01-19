54c61d10a310e82213cf7667a27e56dac48dfc5a
Wellington Gonçalves Pires <wellington.goncalves@live.com>
2025-01-19 12:14:18 -0300

    Added 'state' field and added Jackson annotation to avoid show null values

--- src/main/java/com/devicemanager/app/dto/DeviceDTO.java
+++ src/main/java/com/devicemanager/app/dto/DeviceDTO.java
@@ -1,9 +1,13 @@
 package com.devicemanager.app.dto;
 
+import com.devicemanager.app.enums.StateEnum;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 
 import java.util.UUID;
 
 @Builder
-public record DeviceDTO(UUID id, String name, String brand) {
+@JsonInclude(JsonInclude. Include. NON_NULL)
+public record DeviceDTO(UUID id, String name, String brand, @JsonProperty("state") StateEnum stateEnum) {
 }
