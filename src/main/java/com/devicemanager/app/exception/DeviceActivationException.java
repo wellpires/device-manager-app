2f0b2d0443f7db6e695778ba25286222cd24c321
Wellington Gonçalves Pires <wellington.goncalves@live.com>
2025-01-20 00:58:06 -0300

    Added custom exception to handle when device state is Inactive

---
+++ src/main/java/com/devicemanager/app/exception/DeviceActivationException.java
@@ -0,0 +1,7 @@
+package com.devicemanager.app.exception;
+
+public class DeviceActivationException extends RuntimeException {
+    public DeviceActivationException(String message) {
+        super(message);
+    }
+}
