c15a658feb53beb4d8d3c21b2a689939fc2d8b10
Wellington Gonçalves Pires <wellington.goncalves@live.com>
2025-01-20 00:20:37 -0300

    Added a constructor overload to set stateId in exception message

--- src/main/java/com/devicemanager/app/exception/DeviceStateNotFoundException.java
+++ src/main/java/com/devicemanager/app/exception/DeviceStateNotFoundException.java
@@ -2,9 +2,15 @@ package com.devicemanager.app.exception;
 
 import com.devicemanager.app.enums.StateEnum;
 
+import java.util.UUID;
+
 public class DeviceStateNotFoundException extends RuntimeException {
 
     public DeviceStateNotFoundException(StateEnum stateEnum) {
         super(String.format("Device status %s not found", stateEnum.name()));
+    }
+
+    public DeviceStateNotFoundException(UUID stateId) {
+        super(String.format("Device status %s not found", stateId));
     }
 }
