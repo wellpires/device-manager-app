42795c8baf4880e285a2c4d4c4bee2d001cb27ba
Wellington Gonçalves Pires <wellington.goncalves@live.com>
2025-01-20 01:24:15 -0300

    Added builder annotation

--- src/main/java/com/devicemanager/app/dto/request/DeviceStateRequest.java
+++ src/main/java/com/devicemanager/app/dto/request/DeviceStateRequest.java
@@ -2,6 +2,8 @@ package com.devicemanager.app.dto.request;
 
 import com.devicemanager.app.dto.DeviceStateDTO;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Builder;
 
+@Builder
 public record DeviceStateRequest (@JsonProperty("device-state") DeviceStateDTO deviceStateDTO) {
 }
